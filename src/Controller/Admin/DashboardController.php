da2de972194985641ca1efb6f62b664a5d3127b0
LucArronne <lucar.tservice@gmail.com>
2025-01-10 22:56:13 +0100

    Creation Dashboard Admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,12 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_ADMIN')]
 class DashboardController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'admin_dashboard')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
+        // L'annotation IsGranted vérifiera automatiquement le rôle
         return $this->render('admin/dashboard.html.twig');
     }
 } 
