c424a5f93a97f40f03436801dd6a4af665ea6cee
LucArronne <lucar.tservice@gmail.com>
2025-01-10 21:52:01 +0100

    Confirmation ajout admin

--- src/Command/CreateAdminCommand.php
+++ src/Command/CreateAdminCommand.php
@@ -4,69 +4,49 @@ namespace App\Command;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Console\Attribute\AsCommand;
 
 #[AsCommand(
     name: 'app:create-admin',
-    description: 'Crée un utilisateur admin'
+    description: 'Crée un utilisateur administrateur'
 )]
 class CreateAdminCommand extends Command
 {
-    protected static $defaultName = 'app:create-admin';
-    protected static $defaultDescription = 'Crée un utilisateur admin';
-
-    private EntityManagerInterface $entityManager;
-    private UserPasswordHasherInterface $passwordHasher;
-
     public function __construct(
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher
     ) {
         parent::__construct();
-        $this->entityManager = $entityManager;
-        $this->passwordHasher = $passwordHasher;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
-        // Vérifier si un admin existe déjà
-        $existingAdmin = $this->entityManager->getRepository(User::class)
-            ->findOneBy(['email' => 'admin@simracing.com']);
-
-        if ($existingAdmin) {
-            $io->warning('Un administrateur existe déjà !');
-            return Command::SUCCESS;
-        }
-
-        // Créer l'admin
         $admin = new User();
         $admin->setEmail('admin@simracing.com');
+        $admin->setRoles(['ROLE_ADMIN']);
         $admin->setFirstName('Admin');
         $admin->setLastName('SimRacing');
-        $admin->setRoles(['ROLE_ADMIN']);
-        
-        // Ajouter la date de création
         $admin->setCreatedAt(new \DateTimeImmutable());
-
+        
         // Hasher le mot de passe
         $hashedPassword = $this->passwordHasher->hashPassword($admin, 'admin');
         $admin->setPassword($hashedPassword);
 
-        // Sauvegarder l'admin
+        // Sauvegarder dans la base de données
         $this->entityManager->persist($admin);
         $this->entityManager->flush();
 
         $io->success('Administrateur créé avec succès !');
         $io->table(
-            ['Email', 'Password', 'Roles'],
-            [['admin@simracing.com', 'admin', 'ROLE_ADMIN']]
+            ['Email', 'Mot de passe'],
+            [['admin@simracing.com', 'admin']]
         );
 
         return Command::SUCCESS;
