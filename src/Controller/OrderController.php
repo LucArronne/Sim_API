5ac4a265a9810d26df6be494982e5c2be37af4b2
LucArronne <lucar.tservice@gmail.com>
2025-01-13 22:11:49 +0100

    Espace client User

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -5,17 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class OrderController extends AbstractController
 {
     #[Route('/orders', name: 'app_orders')]
     public function index(): Response
     {
-        if (!$this->getUser()) {
-            return $this->redirectToRoute('app_login');
-        }
         return $this->render('orders/index.html.twig');
     }
 } 
