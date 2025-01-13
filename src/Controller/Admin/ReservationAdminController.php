43773434fa29e3cf7653893eb9add1bbe92c1457
LucArronne <lucar.tservice@gmail.com>
2025-01-13 14:06:52 +0100

    Gerer les Resa Admin

--- src/Controller/Admin/ReservationAdminController.php
+++ src/Controller/Admin/ReservationAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Reservation;
+use App\Entity\Disponibilite;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -69,5 +70,46 @@ class ReservationAdminController extends AbstractController
                 'message' => 'Erreur lors de la suppression'
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
+
+    #[Route('/admin/disponibilites/create', name: 'admin_create_disponibilite', methods: ['POST'])]
+    public function createDisponibilite(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        try {
+            $data = json_decode($request->getContent(), true);
+
+            $disponibilite = new Disponibilite();
+            $disponibilite->setDateDebut(new \DateTime($data['dateDebut']));
+            $disponibilite->setDateFin(new \DateTime($data['dateFin']));
+            $disponibilite->setTitle($data['title'] ?? 'Créneau de simulation');
+            $disponibilite->setDescription($data['description'] ?? null);
+            $disponibilite->setIsAvailable(true);
+
+            $entityManager->persist($disponibilite);
+            $entityManager->flush();
+
+            return $this->json([
+                'success' => true,
+                'message' => 'Disponibilité créée avec succès'
+            ], Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return $this->json([
+                'success' => false,
+                'message' => 'Erreur lors de la création : ' . $e->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    #[Route('/admin/disponibilites', name: 'admin_disponibilites', methods: ['GET'])]
+    public function disponibilites(EntityManagerInterface $entityManager): Response
+    {
+        $disponibilites = $entityManager->getRepository(Disponibilite::class)->findBy(
+            [],
+            ['dateDebut' => 'ASC']
+        );
+
+        return $this->render('admin/reservations/disponibilites.html.twig', [
+            'disponibilites' => $disponibilites
+        ]);
     }
 } 
