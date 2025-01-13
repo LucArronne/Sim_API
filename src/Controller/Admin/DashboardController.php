7b930a5ace5d78ec5eb10a82b0b80b65a5562394
LucArronne <lucar.tservice@gmail.com>
2025-01-13 01:21:31 +0100

    Fix erreur redirection dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,14 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/admin/dashboard', name: 'admin_dashboard')]
+    #[Route('/admin/dashboard', name: 'admin_dashboard', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_ADMIN')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        // L'annotation IsGranted vérifiera automatiquement le rôle
+        $token = $request->request->get('_token');
+        if ($token) {
+            // Le token est présent dans la requête POST
+            return $this->render('admin/dashboard.html.twig', [
+                'token' => $token
+            ]);
+        }
+
         return $this->render('admin/dashboard.html.twig');
     }
 } 
